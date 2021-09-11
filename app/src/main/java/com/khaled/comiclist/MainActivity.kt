993623a8bfd958ad5424b4cbf34eea3aac1a3654
KHALED-PC\Khaled Ismail <khaled.i.aboshama@gmail.com>
2021-09-11 19:02:18 +0200

    Add toolbar and navigation graph

--- app/src/main/java/com/khaled/comiclist/MainActivity.kt
+++ app/src/main/java/com/khaled/comiclist/MainActivity.kt
@@ -2,12 +2,26 @@ package com.khaled.comiclist
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.khaled.comiclist.common.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<MainViewModel>() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigationGraph()
+    }
+
+    private fun setupNavigationGraph() {
+        navController = findNavController(R.id.mainNavHostFragment)
+        mainBottomNavigationView.setOnNavigationItemReselectedListener {}
+        NavigationUI.setupWithNavController(mainBottomNavigationView, navController)
     }
 
     override val loadingView: View?
